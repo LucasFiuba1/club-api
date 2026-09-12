4d664ed9c6c6b9f2f2d506918ac92f389bcc8a98
LucasFiuba1 <lcirillo@fi.uba.ar>
2026-09-12 18:09:56 -0300

    feat(routes): get socio by id route

---
+++ src/routes/socios/socio_routes.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, jsonify
+
+from services.socios.socio_service import get_socio
+
+socio_routes = Blueprint(
+    "socios",
+    __name__,
+    url_prefix="/socios"
+)
+
+@socio_routes("</int:socio_id>")
+def get_socio_route(socio_id):
+    socio, error, status = get_socio(socio_id)
+
+    if error:
+        return jsonify(error), status
+
+    return jsonify(socio), status
