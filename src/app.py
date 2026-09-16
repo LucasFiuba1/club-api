904ccceaac9c4a725e2e0bd33f17496d0c20e6d7
FinKedinn1 <finkedinnze@gmail.com>
2026-09-16 15:55:36 -0300

    feat(routes): expose GET endpoints 4 deportes and canchas

--- src/app.py
+++ src/app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 
 from routes.socios import socio_routes
-
+from routes.canchas import cancha_routes
+from routes.deportes import deporte_routes
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(socio_routes)
+    app.register_blueprint(cancha_routes)
+    app.register_blueprint(deporte_routes)
 
     return app
 
