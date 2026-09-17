1d014696c0b59aa079abefad5e8ef4d007ef2945
LucasFiuba1 <lcirillo@fi.uba.ar>
2026-09-16 21:54:23 -0300

    feat(scripts): create deportes table and insert examples

--- scripts/init_db.sql
+++ scripts/init_db.sql
@@ -1,3 +1,17 @@
 CREATE DATABASE IF NOT EXISTS club_deportivo;
 
 USE club_deportivo;
+
+/* DEPORTES */
+CREATE TABLE
+    deportes (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        nombre VARCHAR(50) NOT NULL UNIQUE
+    );
+
+INSERT INTO
+    deportes (nombre)
+VALUES
+    ('Fútbol'),
+    ('Tenis'),
+    ('Pádel');
