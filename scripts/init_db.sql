80508b1861d1c361a874c753e6be4f9828f59821
LucasFiuba1 <lcirillo@fi.uba.ar>
2026-09-16 22:03:06 -0300

    fix(scripts): table and inserts inconsistences

--- scripts/init_db.sql
+++ scripts/init_db.sql
@@ -30,14 +30,12 @@ CREATE TABLE
     );
 
 INSERT INTO
-    canchas (nombre, precio_hora, techada, activa)
+    canchas (nombre, id_deporte, precio_hora, techada, activa)
 VALUES
-    (
-        ('Cancha de fútbol 1', 1, 1000000, FALSE, TRUE),
-        ('Cancha de Tenis 2', 2, 500000, FALSE, TRUE),
-        ('Cancha de Pádel 3', 3, 700000, TRUE, TRUE),
-        ('Cancha de Pádel 3', 3, 650000, FALSE, FALSE)
-    );
+    ('Cancha de fútbol 1', 1, 1000000, FALSE, TRUE),
+    ('Cancha de Tenis 2', 2, 500000, FALSE, TRUE),
+    ('Cancha de Pádel 3', 3, 700000, TRUE, TRUE),
+    ('Cancha de Pádel 4', 3, 650000, FALSE, FALSE);
 
 -----------------    
 /* SOCIOS */
@@ -46,33 +44,31 @@ CREATE TABLE
         id INT PRIMARY KEY AUTO_INCREMENT,
         nombre VARCHAR(100) NOT NULL,
         email VARCHAR(100) NOT NULL UNIQUE,
-        activo BOOLEAN NOT NULL DEFAULT TRUE,
+        activo BOOLEAN NOT NULL DEFAULT TRUE
     );
 
 INSERT INTO
-    TABLE socios (nombre, email, activo)
+    socios (nombre, email, activo)
 VALUES
     (
-        (
-            'Lucas Cirillo Berardi',
-            'lucascirilloberardi@gmail.com',
-            TRUE
-        ),
-        ('Joaquin', 'joaquin@gmail.com', TRUE),
-        ('Marteo', 'mateo@gmail.com', FALSE),
-        ('Roman', 'roman@gmail.com', TRUE),
-        ('Sergio', 'sergio@gmail.com', TRUE)
-    );
+        'Lucas Cirillo Berardi',
+        'lucascirilloberardi@gmail.com',
+        TRUE
+    ),
+    ('Joaquin', 'joaquin@gmail.com', TRUE),
+    ('Mateo', 'mateo@gmail.com', FALSE),
+    ('Roman', 'roman@gmail.com', TRUE),
+    ('Sergio', 'sergio@gmail.com', TRUE);
 
 -----------------    
 /* RESERVAS */
 CREATE TABLE
     reservas (
         id INT PRIMARY KEY AUTO_INCREMENT,
-        id_socio INT PRIMARY KEY AUTO_INCREMENT,
-        id_cancha INT PRIMARY KEY AUTO_INCREMENT,
-        fecha_hora_inicio DATETIME NOT NULL,
-        fecha_hora_fin DATETIME NOT NULL,
+        id_socio INT NOT NULL,
+        id_cancha INT NOT NULL,
+        fecha_hora_inicio DATETIME (6) NOT NULL,
+        fecha_hora_fin DATETIME (6) NOT NULL,
         estado VARCHAR(20) NOT NULL DEFAULT 'confirmada',
         tarifa_hora INT NOT NULL,
         total INT NOT NULL,
@@ -94,8 +90,8 @@ VALUES
     (
         1,
         1,
-        '2026-10-15 18:00:00',
-        '2026-10-15 20:00:00',
+        '2026-10-15 18:00:00.121000',
+        '2026-10-15 20:00:00.121000',
         'confirmada',
         1000000,
         2000000
