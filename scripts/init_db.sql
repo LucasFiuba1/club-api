df31d0004d3886034406f3c3591b12ca1d8a39c1
mateo gelardi <mgelardi@fi.uba.ar>
2026-09-22 20:05:39 -0300

    feat(reservas): modulo complto con capas repository, service y routes validados

--- scripts/init_db.sql
+++ scripts/init_db.sql
@@ -1,3 +1,37 @@
 CREATE DATABASE IF NOT EXISTS club_deportivo;
 
 USE club_deportivo;
+
+CREATE TABLE deportes (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL
+);
+
+
+
+CREATE TABLE canchas (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL, 
+    id_deporte INT NOT NULL,
+    precio_hora INT NOT NULL,
+    techada BOOLEAN NOT NULL,
+    activa BOOLEAN NOT NULL,
+    FOREIGN KEY (id_deporte) REFERENCES deportes(id)
+);
+
+CREATE TABLE IF NOT EXISTS reservas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_socio INT NOT NULL,
+    id_cancha INT NOT NULL,
+    fecha_hora_inicio DATETIME NOT NULL,
+    fecha_hora_fin DATETIME NOT NULL, 
+    estado VARCHAR(50) NOT NULL DEFAULT 'confirmada',
+    tarifa_hora BIGINT NOT NULL,
+    importe_total BIGINT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_socio) REFERENCES socios(id),
+    FOREIGN KEY (id_cancha) REFERENCES canchas(id)
+);
+/*fila de prueba*/
+INSERT INTO reservas(id_socio, id_cancha, fecha_hora_inicio, fecha_hora_fin, estado, tarifa_hora, importe_total)
+VALUES (1, 2, '2026-10-15 18:00:00', '2026-10-15 20:00:00', 'confirmada', 1000000, 2000000);
--- src/app.py
+++ src/app.py
@@ -1,16 +1,17 @@
 from flask import Flask
 
-from routes.socios import socio_routes
+from routes.reservas.reservas_routes import reserva_bp
+from routes.socios.socio_routes import socio_routes
 
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(socio_routes)
-
+    app.register_blueprint(reserva_bp)
     return app
 
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
