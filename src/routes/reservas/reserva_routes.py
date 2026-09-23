988b77681baf4fff6994de1535c893f7bcd6652a
LucasFiuba1 <lcirillo@fi.uba.ar>
2026-09-23 20:49:59 -0300

    fix: conflicts and imports

--- src/routes/reservas/reserva_routes.py
+++ src/routes/reservas/reserva_routes.py
@@ -1,10 +1,52 @@
 from flask import Blueprint, jsonify, request
 
-from services.reservas.reserva_service import create_reserva_service
-from validators.reserva_validator import validate_reserva
-from utils.error_utils import build_error
+from src.services.reservas.reservas_service import (
+    cambiar_estado_reserva,
+    create_reserva_service,
+    listar_reservas,
+    obtener_reserva,
+)
+from src.utils.error_utils import build_error
+from src.validators.reserva_validator import validate_reserva
 
 reserva_routes=Blueprint("reservas", __name__, url_prefix="/reservas")
+
+@reserva_routes.route('/reservas', methods=['GET'])
+def get_reserva():
+
+    limit = int(request.args.get('_limit', 10))
+    offset = int(request.args.get('_offset', 0))
+
+    filters = {
+        'id_cancha': request.args.get('id_cancha'),
+        'id_socio': request.args.get('id_socio'),
+        'estado': request.args.get('estado'),
+        'fecha_desde': request.args.get('fecha_desde'),
+        'fecha_hasta': request.args.get('fecha_hasta')
+    }
+
+    reservas, total = listar_reservas(filters, limit, offset)
+
+    links = {
+        "_first": f"/reservas?_limit={limit}&_offset=0",
+        "_prev": f"/reservas?_limit={limit}&_offset={max(0, offset - limit)}" if offset > 0 else None,
+        "_next": f"/reservas?_limit={limit}&_offset={offset + limit}" if offset + limit < total else None,
+        "_last": f"/reservas?_limit={limit}&_offset={max(0, total - limit)}"
+    }
+
+    return jsonify({
+        'reservas': reservas,
+        '_links': links
+    }), 200
+
+
+@reserva_routes.route('/reservas/<int:reserva_id>', methods=['GET'])
+def get_reserva_by_id(reserva_id):
+    reserva = obtener_reserva(reserva_id)
+    if not reserva:
+        return jsonify({'error': 'Reserva no encontrada'}), 404
+
+    return jsonify(reserva), 200
 
 @reserva_routes.route("/", methods=["POST"])
 def create_reserva_route():
@@ -24,3 +66,20 @@ def create_reserva_route():
         return jsonify(error), status
 
     return jsonify(reserva), status
+
+
+@reserva_routes.route('/reservas/<int:reserva_id>/estado', methods=['PUT'])
+def update_estado(reserva_id):
+    """
+    Endpoint paraactualizar el estado de una reserva por su ID
+    """
+    data = request.get_json()
+
+    if not data or 'estado' not in data:
+        return jsonify({'error': 'El campo estado es obligatorio'}), 400
+
+    resultado = cambiar_estado_reserva(reserva_id, data['estado'])
+
+    if 'error' in resultado:
+        return jsonify({'error': resultado['error']}), resultado ['code']
+    return jsonify(resultado['data']), resultado['code']
